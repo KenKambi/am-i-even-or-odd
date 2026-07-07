c4a4f2ca60ff678a1bb292af23c973d801046b5d
Ken Kambi <kenkambi43@gmail.com>
2026-07-07 18:59:04 +0300

    setup

---
+++ am-i-even-or-odd/__init__.py
@@ -0,0 +1,8 @@
+from .checker import is_it_odd_tho, is_it_even_tho
+
+__version__ = "0.1.0"
+
+__all__ = [
+    "is_it_odd_tho",
+    "is_it_even_tho",
+]
---
+++ am-i-even-or-odd/checker.py
@@ -0,0 +1,7 @@
+def is_it_even_tho(number: int) -> bool:
+    """Return True if number is even."""
+    return number % 2 == 0
+
+def is_it_odd_tho(number: int) -> bool:
+    """Return True if number is odd."""
+    return number % 2 != 0
